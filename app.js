cea9605d89d22e756484716786ee4ae830dc62d1
Ethan Burnet <twirlyseal@gmail.com>
2024-06-06 14:21:58 +1000

    Fixes + improvements + non-functional loading overlay

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 chrome.tabs.onCreated.addListener(injector);
 chrome.tabs.onUpdated.addListener(injector);
 
+injected = []
+
 function injector(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    if (tab.url && tab.url.startsWith("https://canvas.")) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId},
-        files: ["inject.js"],
-    })
+  if (tab.url && tab.url.startsWith("https://canvas.")) {
+    if (changeInfo.status !== 'complete') {
+      if (!injected.includes(tabId)) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabId},
+          files: ["inject.js"],
+          injectImmediately: true
+        })
+        injected.push(tabId)
+        console.log("Injected")
+      }
+    } else if (changeInfo.status === 'complete') {
+      injected = injected.filter(item => item !== tabId);
     }
   }
 }
