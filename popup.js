e4007bbf43565ca99f7aea25f335ccbd6463651d
Ethan Burnet <twirlyseal@gmail.com>
2024-06-06 14:21:58 +1000

    Nightshade for Canvas

--- popup.js
+++ popup.js
@@ -2,18 +2,12 @@ document.querySelector('#saveButton').addEventListener('click', function() {
     var urlInput = document.querySelector('#urlInput').value;
 
     if (!urlInput.startsWith("https://")) {
-        urlInput = "https://" + urlInput
+        urlInput = "https://" + urlInput;
     }
 
     if (!urlInput.endsWith("/")) {
         urlInput += "/";
     }
 
-    console.log(urlInput)
-
-    chrome.storage.local.set({ 'canvasURL': urlInput })
-
-    chrome.storage.local.get("canvasURL", (items) => {
-        console.log(Object.entries(items)[0][1]);
-    });
+    chrome.storage.local.set({ 'canvasURL': urlInput });
 });
