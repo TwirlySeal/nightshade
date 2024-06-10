c70a680ea78603f03b1629df2ef10b06f14979fa
Ethan Burnet <ethanandrewburnet@gmail.com>
2024-06-10 13:03:52 +1000

    Reorganised functions

--- js/content.js
+++ js/content.js
@@ -1,87 +1,3 @@
-function loading() {
-    const overlay = document.createElement("div")
-    overlay.style.cssText = `
-        background-color: #1c1c1c;
-        top: 0;
-        right: 0;
-        left: 0;
-        bottom: 0;
-        z-index: 100;
-        position: fixed;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    `
-    overlay.id = "loading-overlay"
-    document.body.appendChild(overlay)
-
-    const icon = document.createElement("img")
-    icon.height = "128"
-    icon.width = "128"
-    icon.src = chrome.runtime.getURL("icons/icon390.png")
-    overlay.appendChild(icon)
-
-    document.addEventListener("readystatechange", function() {
-        if (document.readyState === "complete") {
-            overlay.remove()
-            searchBar()
-            document.removeEventListener("readystatechange", this);
-        }
-    });
-}
-
-function insertCSS() {
-    const linkElement = document.createElement("link");
-    linkElement.rel = "stylesheet";
-    linkElement.type = "text/css";
-    linkElement.href = chrome.runtime.getURL("css/main.css");
-    document.head.appendChild(linkElement);
-}
-
-// Function not used
-function elementWait(elementClass, callback) {
-    const observer = new MutationObserver((mutationsList, observer) => {
-        for (let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                mutation.addedNodes.forEach(node => {
-                    if (node.toString().includes(elementClass)) {
-                        observer.disconnect();
-                        callback();
-                    }
-                });
-            }
-        }
-    });
-
-    observer.observe(document.body, { childList: true, subtree: true });
-}
-
-function searchBar() {
-    const host = document.querySelector('#ajas-search-widget');
-    if (host) {
-        var sheet = new CSSStyleSheet();
-        sheet.replaceSync(`
-            #ajas-search01 {
-            background-color: rgb(60, 60, 60);
-            border: 1px solid rgb(70, 70, 70);
-            color: #FFFFFF;
-            border-radius: 4px;
-            }
-
-            .ajas-search-widget--dashboard {
-            margin: 0;
-            }
-
-            input:-webkit-autofill, input:-webkit-autofill:focus {
-            transition: background-color 0s 600000s, color 0s 600000s !important;
-            }
-
-        `);
-        host.shadowRoot.adoptedStyleSheets.push(sheet);
-    }
-}
-
 async function dashboard(coursesPromise) {
     let dashboardElement
 
@@ -219,43 +135,6 @@ function calendar() {
         `;
     });
 }
-
-// Wait for body element
-function bodyWait() {
-    const observer = new MutationObserver(() => {
-        if (document.body !== null) {
-            observer.disconnect();
-            loading();
-
-            coursesPromise = fetch(window.location.origin + '/api/v1/courses/');
-            setupCourseNav(coursesPromise);
-            navBar();
-
-            document.addEventListener("DOMContentLoaded", insertCSS);
-        
-            if (window.location.pathname === '/') {
-                dashboard(coursesPromise);
-            } else if (window.location.pathname.startsWith('/courses') || window.location.pathname.startsWith('/groups') || window.location.pathname.startsWith('/profile')) {
-                contentLayout();
-            } else if (window.location.pathname.startsWith("/calendar")) {
-                calendar;
-            }
-        }
-    });
-    
-    observer.observe(document.documentElement, {
-        childList: true,
-        subtree: true
-    });
-}
-
-// Check if Canvas URL
-var currentUrl = window.location.hostname
-chrome.storage.local.get("canvasURL", (items) => {
-    if (currentUrl === new URL(Object.entries(items)[0][1]).hostname) {
-        bodyWait()
-    }
-});
 
 async function setupCourseNav(coursesPromise) {
     // Setup alpine.js
@@ -416,7 +295,122 @@ function navBar() {
     coursesButton.setAttribute('x-on:click', 'sidebar = ! sidebar');
 }
 
-function toggleSidebar() {
-    let courseNav = document.querySelector("#course-nav");
-    courseNav.classList.toggle('active');
+function searchBar() {
+    const host = document.querySelector('#ajas-search-widget');
+    if (host) {
+        var sheet = new CSSStyleSheet();
+        sheet.replaceSync(`
+            #ajas-search01 {
+            background-color: rgb(60, 60, 60);
+            border: 1px solid rgb(70, 70, 70);
+            color: #FFFFFF;
+            border-radius: 4px;
+            }
+
+            .ajas-search-widget--dashboard {
+            margin: 0;
+            }
+
+            input:-webkit-autofill, input:-webkit-autofill:focus {
+            transition: background-color 0s 600000s, color 0s 600000s !important;
+            }
+
+        `);
+        host.shadowRoot.adoptedStyleSheets.push(sheet);
+    }
 }
+
+function loading() {
+    const overlay = document.createElement("div")
+    overlay.style.cssText = `
+        background-color: #1c1c1c;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        z-index: 100;
+        position: fixed;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    `
+    overlay.id = "loading-overlay"
+    document.body.appendChild(overlay)
+
+    const icon = document.createElement("img")
+    icon.height = "128"
+    icon.width = "128"
+    icon.src = chrome.runtime.getURL("icons/icon390.png")
+    overlay.appendChild(icon)
+
+    document.addEventListener("readystatechange", function() {
+        if (document.readyState === "complete") {
+            overlay.remove()
+            searchBar()
+            document.removeEventListener("readystatechange", this);
+        }
+    });
+}
+
+function insertCSS() {
+    const linkElement = document.createElement("link");
+    linkElement.rel = "stylesheet";
+    linkElement.type = "text/css";
+    linkElement.href = chrome.runtime.getURL("css/main.css");
+    document.head.appendChild(linkElement);
+}
+
+// Function not used
+function elementWait(elementClass, callback) {
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(node => {
+                    if (node.toString().includes(elementClass)) {
+                        observer.disconnect();
+                        callback();
+                    }
+                });
+            }
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+}
+
+function bodyWait() {
+    const observer = new MutationObserver(() => {
+        if (document.body !== null) {
+            observer.disconnect();
+            loading();
+
+            coursesPromise = fetch(window.location.origin + '/api/v1/courses/');
+            setupCourseNav(coursesPromise);
+            navBar();
+
+            document.addEventListener("DOMContentLoaded", insertCSS);
+        
+            if (window.location.pathname === '/') {
+                dashboard(coursesPromise);
+            } else if (window.location.pathname.startsWith('/courses') || window.location.pathname.startsWith('/groups') || window.location.pathname.startsWith('/profile')) {
+                contentLayout();
+            } else if (window.location.pathname.startsWith("/calendar")) {
+                calendar;
+            }
+        }
+    });
+    
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+}
+
+// Check if Canvas URL
+var currentUrl = window.location.hostname
+chrome.storage.local.get("canvasURL", (items) => {
+    if (currentUrl === new URL(Object.entries(items)[0][1]).hostname) {
+        bodyWait()
+    }
+});
