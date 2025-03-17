9c8360d586808bdf45bb790bf93ecd737921da21
justinac0 <justinac.1999@gmail.com>
2025-03-17 20:14:56 +1000

    Support for specifying manifest build target.

--- package/main.py
+++ package/main.py
@@ -1,95 +1,134 @@
+import sys
 import kdl
 import json
 from pprint import pprint
 
-with open('manifest.kdl') as f: s = f.read()
-doc = kdl.parsefuncs.parse(s)
+TARGET_FIREFOX = 'firefox'
+TARGET_CHROME = 'chrome'
 
-manifest = {}
+DEFINE_BUILDDIR = '$BUILD_DIR'
 
-for node in doc.nodes:
-    match node.name:
-        case 'manifest-version':
-            manifest['manifest_version'] = node.args[0]
+def match_replace_define(source: str, define: str, value: str):
+    if source.find(define) != -1:
+        return str.replace(source, define, value)
 
-        case 'icons':
-            icons = {}
-            for icon in node.nodes:
-                icons[icon.name] = icon.args[0]
+    return source
 
-            manifest['icons'] = icons
+def build_manifest(target: str):
+    with open('manifest.kdl') as f: s = f.read()
+    doc = kdl.parsefuncs.parse(s)
 
-        case 'permissions':
-            manifest['permissions'] = [perm.name for perm in node.nodes]
+    manifest = {}
 
-        case 'defaults': # action
-            # Does not support 'default_icon'
-            action = {}
+    for node in doc.nodes:
+        match node.name:
+            case 'manifest-version':
+                manifest['manifest_version'] = node.args[0]
 
-            for default in node.nodes:
-                key = ""
-                match default.name:
-                    case 'popup':
-                        key = 'default_popup'
-                    case 'tooltip':
-                        key = 'default_title'
+            case 'icons':
+                icons = {}
+                for icon in node.nodes:
+                    icons[icon.name] = icon.args[0]
 
-                action[key] = default.args[0]
+                manifest['icons'] = icons
 
-            manifest['action'] = action
+            case 'permissions':
+                manifest['permissions'] = [perm.name for perm in node.nodes]
 
-        case 'injections': # content_scripts
-            # Only supports single-value fields
-            injections = []
-            for match in node.nodes:
-                obj = {}
+            case 'defaults': # action
+                # Does not support 'default_icon'
+                action = {}
 
-                for field in match.nodes:
+                for default in node.nodes:
                     key = ""
-                    match field.name:
-                        case 'run-at':
-                            obj['run_at'] = field.args[0]
-                            continue
-                        case 'match':
-                            key = 'matches'
-                        case 'script':
-                            key = 'js'
+                    match default.name:
+                        case 'popup':
+                            key = 'default_popup'
+                        case 'tooltip':
+                            key = 'default_title'
 
-                    obj[key] = field.args
+                    action[key] = default.args[0]
 
-                injections.append(obj)
+                manifest['action'] = action
 
-            manifest['content_scripts'] = injections
+            case 'injections': # content_scripts
+                # Only supports single-value fields
+                injections = []
+                for match in node.nodes:
+                    obj = {}
 
-        case 'background-script': # background
-            # Does not support type field
-            manifest['background'] = {"service_worker": node.args[0]}
+                    for field in match.nodes:
+                        key = ""
+                        match field.name:
+                            case 'run-at':
+                                obj['run_at'] = field.args[0]
+                                continue
+                            case 'match':
+                                key = 'matches'
+                            case 'script':
+                                key = 'js'
 
-        case 'resources': # web_accessible_resources
-            # Does not support 'use_dynamic_url'
-            # To-do: make files the parent key for matches and extension_ids to allow for both
-            resources = []
-            for resource in node.nodes:
-                obj = {}
+                        # match args against defs and update the values
+                        updatedargs = []
+                        for arg in field.args:
+                            v = match_replace_define(arg, DEFINE_BUILDDIR, target)
+                            if v != arg: # success
+                                v = f'build/{v}'
 
-                for field in resource.nodes:
-                    key = ""
-                    match field.name:
-                        case 'files':
-                            key = 'resources'
-                        case 'match':
-                            key = 'matches'
+                            updatedargs.append(v)
 
-                    obj[key] = field.args
+                        obj[key] = updatedargs
 
-                resources.append(obj)
+                    injections.append(obj)
 
-            manifest['web_accessible_resources'] = resources
+                manifest['content_scripts'] = injections
 
-        case _:
-            try:
-                manifest[node.name] = node.args[0]
-            except:
-                pprint(node)
+            case 'background-script': # background
+                # Does not support type field
+                manifest['background'] = {"service_worker": node.args[0]}
 
-with open("../manifest.json", "w") as f: json.dump(manifest, f, indent=2)
+            case 'resources': # web_accessible_resources
+                # Does not support 'use_dynamic_url'
+                # To-do: make files the parent key for matches and extension_ids to allow for both
+                resources = []
+                for resource in node.nodes:
+                    obj = {}
+
+                    for field in resource.nodes:
+                        key = ""
+                        match field.name:
+                            case 'files':
+                                key = 'resources'
+                            case 'match':
+                                key = 'matches'
+
+                        obj[key] = field.args
+
+                    resources.append(obj)
+
+                manifest['web_accessible_resources'] = resources
+
+            case _:
+                try:
+                    manifest[node.name] = node.args[0]
+                except:
+                    pprint(node)
+
+    with open("../manifest.json", "w") as f: json.dump(manifest, f, indent=2)
+
+def run(args: list[str]):
+    if len(args) != 2: # only supporting firefox and chrome
+        print(f'expected 2 arguments, recieved {len(args)}')
+        return
+
+    TARGET = args[1]
+    if TARGET == TARGET_CHROME:
+        build_manifest(TARGET_CHROME)
+    elif TARGET == TARGET_FIREFOX:
+        build_manifest(TARGET_FIREFOX)
+    else:
+        print('could not build manifest for specified target...')
+
+
+if __name__ == '__main__':
+    run(sys.argv)
