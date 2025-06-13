f9b657d173ed2f5f85e6da1d4132c15e10346d6c
Ethan Burnet <ethanandrewburnet@gmail.com>
2025-06-13 16:26:56 +1000

    main.ts tweaks

--- src/injected/main.ts
+++ src/injected/main.ts
@@ -1,28 +1,5 @@
 import { dashboard } from "./dashboard.ts";
 
-async function main() {
-  const urlSetting = (await browser.storage.local.get("canvasURL")).canvasURL;
-  if (window.location.hostname != urlSetting) return;
-
-  const coursesPromise = fetchCourses();
-
-  const contentLayout = () => console.log("content layout");
-  const routes = {
-    "": () => dashboard(coursesPromise),
-
-    "courses": contentLayout,
-    "groups": contentLayout,
-    "profile": contentLayout,
-
-    "calendar": () => console.log("calendar")
-  }
-
-  const firstPathSegment = /\/([^/]*)/.exec(location.pathname)![1] as keyof typeof routes;
-  return routes[firstPathSegment]();
-}
-
-main();
-
 async function fetchCourses(): Promise<Response[]> {
   let url = location.origin + "/api/v1/courses?include[]=term&include[]=tabs";
   const regex = /<([^>]+)>; rel="next/i;
@@ -43,3 +20,26 @@ async function fetchCourses(): Promise<Response[]> {
 
   return courses;
 }
+
+async function main() {
+  const urlSetting = (await browser.storage.local.get("canvasURL")).canvasURL;
+  if (location.hostname != urlSetting) return;
+
+  const coursesPromise = fetchCourses();
+
+  const contentLayout = () => console.log("content layout");
+  const routes = {
+    "": () => dashboard(coursesPromise),
+
+    "courses": contentLayout,
+    "groups": contentLayout,
+    "profile": contentLayout,
+
+    "calendar": () => console.log("calendar")
+  };
+
+  const firstPathSegment = /\/([^/]*)/.exec(location.pathname)![1] as keyof typeof routes;
+  return routes[firstPathSegment]();
+}
+
+main();
